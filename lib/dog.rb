207c92f9fec22fa4e88fda6b4037707e9cf1c521
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-20 20:34:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,27 @@
 class Dog
-  attr_accessor :name
-  
-  @@all=[]
+    attr_accessor :name
 
-  def initialize(name)
-    @name=name
-    @@all << self
-  end
+    @@all = []
 
-  def self.clear_all
-    @@all.clear
-  end
+    def initialize(name)
+        @name = name
+        self.save
+    end
 
-  def self.all
-    @@all.each {|dog| puts "#{dog.name}"}
-    
-  end
-  
-  def self.print_all
-    # @@all.each do |dog|
-    #   puts dog
-    # end
-    returns self.to_s
-  end
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        @@all.each {|dog| puts dog.name}
+    end
+
+    def save
+        self.class.all << self
+    end
+
 end
